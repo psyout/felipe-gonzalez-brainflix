e17b17c2c34880eb8bb2b1dd76882c3f9dcc06a2
Felipe Gonzalez <fgonzalezcare@gmail.com>
2023-03-18 12:33:14 -0700

    add funcionality to the form component that write the post submittion into the json file

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,64 +1,63 @@
-import Hero from '../components/Hero/Hero';
-import Header from '../components/Header/Header';
-import MainContent from '../components/MainContent/MainContent';
-import AsideContent from '../components/Aside/Aside';
-import { useState, useEffect } from 'react';
-import { useParams } from 'react-router-dom';
-import axios from 'axios';
-export const apiUrl = "https://project-2-api.herokuapp.com";
-export const apiKey = "0aeb84f0-4ddc-452d-96c3-43be45d310de";
+import Hero from "../components/Hero/Hero";
+import Header from "../components/Header/Header";
+import MainContent from "../components/MainContent/MainContent";
+import AsideContent from "../components/Aside/Aside";
+import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import axios from "axios";
+export const apiUrl = "http://localhost:3001";
 
 function Home() {
-  
-  const [videoList, setVideoList] = useState([]);
-  const [selectedVideo, setSelectedVideo] = useState({});
+   const [videoList, setVideoList] = useState([]);
+   const [selectedVideo, setSelectedVideo] = useState({});
 
-  let {id} = useParams()
+   let { id } = useParams();
 
-  function getVideoData (){
-    axios.get(`${apiUrl}/videos?api_key=${apiKey}`)
-      .then((response) => {
-        // console.log(response)
-        setVideoList(response.data);
-        if(id === undefined) {
-          id = response.data[0].id;
-        }
-        getVideoDetails();
-      })
-      .catch(error => console.log(error));
-  }
+   function getVideoData() {
+      axios
+         .get(`${apiUrl}/videos`)
+         .then((response) => {
+            console.log(response);
+            setVideoList(response.data);
+            if (id === undefined) {
+               id = response.data[0].id;
+            }
+            getVideoDetails();
+         })
+         .catch((error) => console.log(error));
+   }
 
-  function getVideoDetails (){
-		axios.get(`${apiUrl}/videos/${id}?api_key=${apiKey}`)
-			.then((response) => { 
-        // console.log(response)
-        setSelectedVideo(response.data)
-      })
-      .catch(error => console.log(error));
-    window.scrollTo(0, 0);
-	}
+   function getVideoDetails() {
+      axios
+         .get(`${apiUrl}/videos/${id}`)
+         .then((response) => {
+            setSelectedVideo(response.data);
+         })
+         .catch((error) => console.log(error));
+      window.scrollTo(0, 0);
+   }
 
-  useEffect(() => {
-    getVideoData();
-  }, []);
+   useEffect(() => {
+      getVideoData();
+   }, []);
 
-	useEffect(()=>{
-		id && getVideoDetails()
-	},[id])
+   useEffect(() => {
+      id && getVideoDetails();
+   }, [id]);
 
-  return (
-    <>
-      <div className="container">
-        <Header />
-      </div>
-      <hr />
-      <Hero selectedVideo={selectedVideo} />
-      <div className="main-container">
-        <MainContent selectedVideo={selectedVideo} />
-        <AsideContent videoList={videoList} selectedVideo={selectedVideo}/>
-      </div>
-    </>
-  );
+   return (
+      <>
+         <div className="container">
+            <Header />
+         </div>
+         <hr />
+         <Hero selectedVideo={selectedVideo} />
+         <div className="main-container">
+            <MainContent selectedVideo={selectedVideo} />
+            <AsideContent videoList={videoList} selectedVideo={selectedVideo} />
+         </div>
+      </>
+   );
 }
 
 export default Home;
--- src/pages/Page/VideoUpload.jsx
+++ src/pages/Page/VideoUpload.jsx
@@ -1,35 +1,104 @@
-import thumb from '../../assets/images/Upload-video-preview.jpg';
-import './VideoUpload.scss';
-// import { Link } from 'react-router-dom';
+import thumb from "../../assets/images/Upload-video-preview.jpg";
+import "./VideoUpload.scss";
+import { useRef, useEffect, useState } from "react";
+import axios from "axios";
+const { v4: uuidv4 } = require("uuid");
+export const apiUrl = "http://localhost:3001";
 
-function VideoUpload(){
-    const handleSubmit = (event) => {
+function VideoUpload() {
+   const formRef = useRef();
+   const [videos, setVideos] = useState([]);
+
+   useEffect(() => {
+      getData();
+   }, []);
+
+   const getData = () => {
+      axios.get(`${apiUrl}/videos`).then((response) => {
+         setVideos(response.data);
+      });
+   };
+
+   const handleSubmit = (event) => {
       event.preventDefault();
-      setTimeout(() => {
-        alert('Your video has been uploaded!');
-        window.location.href = '/';
-      }, 1000);
-    };
-  
-    return(
+      const { title, description } = formRef.current;
+
+      const newVideo = {
+         id: uuidv4(),
+         title: title.value,
+         channel: "Felipe Gonzalez",
+         image: "https://picsum.photos/2000/1000",
+         description: description.value,
+         views: "400",
+         likes: "10",
+         duration: "0:00",
+         video: "https://project-2-api.herokuapp.com/stream",
+         timestamp: Date.now(),
+         comments: [],
+      };
+
+      axios
+         .post(`${apiUrl}/videos`, newVideo)
+         .then((response) => {
+            setVideos([...videos, newVideo]);
+            window.alert("Video posted successfully!");
+         })
+         .catch((err) => {
+            console.log(err);
+         });
+
+      event.target.reset();
+   };
+
+   return (
       <div className="video-container">
-        <h1 className="video-container__title">Upload Video</h1>
-        <h4 className="video-container__subtitle">Video Thumbnail</h4>
-        <form onSubmit={handleSubmit} className="video-form">
-          <img className="input-container__thumb" src={thumb} alt="upload thumbnail" />
-          <div className="input-container">
-            <label className="input-container__label">Title your video</label>
-            <input className="input-container__input" type="text" placeholder="Add a title to your video" />
-            <label className="input-container__label">Add a video description</label>
-            <textarea className="input-container__textarea" type="text" id="comment" name="comment" required placeholder="Add a description to your video"></textarea>
-          </div>
-          <div className="video-footer">
-            <button className="video-footer__button video-footer__button--title" type="submit">Publish</button>
-            <button className="video-footer__button video-footer__button--cancel" type="button">Cancel</button>
-          </div>
-        </form>
+         <h1 className="video-container__title">Upload Video</h1>
+         <h4 className="video-container__subtitle">Video Thumbnail</h4>
+         <form ref={formRef} onSubmit={handleSubmit} className="video-form">
+            <img
+               className="input-container__thumb"
+               src={thumb}
+               alt="upload thumbnail"
+            />
+            <div className="input-container">
+               <label className="input-container__label">
+                  Title your video
+               </label>
+               <input
+                  className="input-container__input"
+                  type="text"
+                  name="title"
+                  placeholder="Add a title to your video"
+               />
+               <label className="input-container__label">
+                  Add a video description
+               </label>
+               <textarea
+                  className="input-container__textarea"
+                  type="text"
+                  name="description"
+                  id="comment"
+                  required
+                  placeholder="Add a description to your video"
+               ></textarea>
+            </div>
+            <div className="video-footer">
+               <button
+                  className="video-footer__button video-footer__button--title"
+                  type="submit"
+               >
+                  Publish
+               </button>
+               <button
+                  className="video-footer__button video-footer__button--cancel"
+                  type="button"
+               >
+                  Cancel
+               </button>
+            </div>
+         </form>
       </div>
-    );
-  }
-  
+   );
+}
+
 export default VideoUpload;
