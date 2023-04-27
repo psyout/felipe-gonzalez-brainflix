d44a58aba83c59485c4d484b4ef0aa6e2a62e39b
Felipe Gonzalez <fgonzalezcare@gmail.com>
2023-04-27 10:11:54 -0700

    adding dependencies to deploy app

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -8,62 +8,62 @@ import axios from 'axios';
 export const apiUrl = 'http://localhost:3001';
 
 function Home() {
-    const [videoList, setVideoList] = useState([]);
-    const [selectedVideo, setSelectedVideo] = useState({});
-    const [defaultVideo, setDefaultVideo] = useState({});
+   const [videoList, setVideoList] = useState([]);
+   const [selectedVideo, setSelectedVideo] = useState({});
+   const [defaultVideo, setDefaultVideo] = useState({});
 
-    let { id } = useParams();
+   let { id } = useParams();
 
-    const getVideoDetails = useCallback(() => {
-        axios
-            .get(`${apiUrl}/videos/${id}`)
+   const getVideoDetails = useCallback(() => {
+      axios
+         .get(`${apiUrl}/videos/${id}`)
+         .then((response) => {
+            setSelectedVideo(response.data);
+         })
+         .catch((error) => console.log(error));
+      window.scrollTo(0, 0);
+   }, [id]);
+
+   useEffect(() => {
+      const getVideoData = () => {
+         axios
+            .get(`${apiUrl}/videos`)
             .then((response) => {
-                setSelectedVideo(response.data);
+               console.log(response);
+               setVideoList(response.data);
+               setDefaultVideo(response.data[0]);
+               if (id === undefined) {
+                  id = response.data[0].id;
+               }
+               getVideoDetails();
             })
             .catch((error) => console.log(error));
-        window.scrollTo(0, 0);
-    }, [id]);
+      };
+      getVideoData();
+   }, [getVideoDetails, id]);
 
-    useEffect(() => {
-        const getVideoData = () => {
-            axios
-                .get(`${apiUrl}/videos`)
-                .then((response) => {
-                    console.log(response);
-                    setVideoList(response.data);
-                    setDefaultVideo(response.data[0]);
-                    if (id === undefined) {
-                        id = response.data[0].id;
-                    }
-                    getVideoDetails();
-                })
-                .catch((error) => console.log(error));
-        };
-        getVideoData();
-    }, [getVideoDetails, id]);
+   useEffect(() => {
+      id && getVideoDetails();
+   }, [getVideoDetails, id]);
 
-    useEffect(() => {
-        id && getVideoDetails();
-    }, [getVideoDetails, id]);
+   function handleLogoClick() {
+      setSelectedVideo(defaultVideo);
+   }
 
-    function handleLogoClick() {
-        setSelectedVideo(defaultVideo);
-    }
+   return (
+      <>
+         <div className='container'>
+            <Header handleLogoClick={handleLogoClick} />
+         </div>
+         <hr />
 
-    return (
-        <>
-            <div className='container'>
-                <Header handleLogoClick={handleLogoClick} />
-            </div>
-            <hr />
-
-            <Hero selectedVideo={selectedVideo} />
-            <div className='main-container'>
-                <MainContent selectedVideo={selectedVideo} />
-                <AsideContent videoList={videoList} selectedVideo={selectedVideo} />
-            </div>
-        </>
-    );
+         <Hero selectedVideo={selectedVideo} />
+         <div className='main-container'>
+            <MainContent selectedVideo={selectedVideo} />
+            <AsideContent videoList={videoList} selectedVideo={selectedVideo} />
+         </div>
+      </>
+   );
 }
 
 export default Home;
